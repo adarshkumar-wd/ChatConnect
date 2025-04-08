d84a8d3f6b8e5356778ca6fcb9ccec4c812b0603
Adarsh Kumar <adarshff06@gmail.com>
2025-04-08 12:06:32 +0530

    Completed the controller which is for Delete message from both user

--- Backend/controllers/deleteMessage.controller.js
+++ Backend/controllers/deleteMessage.controller.js
@@ -1,4 +1,6 @@
+import mongoose from "mongoose";
 import { deletedMessageModdel } from "../models/deletedMessages.model.js";
+import { conversationModel } from "../models/conversation.model.js";
 
 export const createmessage = async (req , res) => {
 
@@ -46,4 +48,26 @@ export const getMessages = async (req , res) => {
 
     return res.status(200).json({success : true , messages : getMessages , message : "Messages fetched successfully..."});
 
+}
+
+export const deleteMessageFromDatabase = async (req , res) => {
+    try {
+
+        const {messageId} = req.params;
+
+        if (!messageId || !mongoose.isValidObjectId) {
+            return res.status(400).json({success : false , message : "Please Provide the valide message Id.."});
+        }
+
+        const deleteMessage = await conversationModel.findByIdAndDelete(messageId);
+        
+        if (!deleteMessage) {
+            return res.status(400).json({success : false , message : "Message not found in the Database"});
+        }
+
+        return res.status(200).json({success : true , message : "Message Deleted Succesfully"});
+
+    } catch (error) {
+        console.log("delete message error : " , error);
+    }
 }
